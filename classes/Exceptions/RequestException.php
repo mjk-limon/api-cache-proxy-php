a48fdc2e4c0372444495dfd003735b124597becc
mjk-limon <jhmasterlimon11@gmail.com>
2025-03-20 15:46:33 +0600

    PALO_API Fix: Corrected typo in rate limit error message

--- classes/Exceptions/RequestException.php
+++ classes/Exceptions/RequestException.php
@@ -7,6 +7,6 @@ class RequestException extends QuintypeException
     protected $errorMessages = [
         400 => 'Bad request.',
         1001 => 'Failed to authenticate.',
-        1003 => 'Rate Limit Exceed.',
+        1003 => 'Rate Limit Exceeded.',
     ];
 }
